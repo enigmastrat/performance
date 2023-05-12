29c24e66aa0d6e7439e63f1bf0b046428e8a26ba
Robert Reed <robbie@cypheye.com>
2023-05-12 11:44:58 -0700

    Partial persistance updates

--- app.py
+++ app.py
@@ -18,7 +18,18 @@ groups = [
 
 acts = [
     {"id": 0, "name": "Like a Prayer", "participants": {"groups":[1], "individuals":[0]}},
-    {"id": 0, "name": "Steppin Time", "participants": {"groups":[0], "individuals":[]}},
+    {"id": 1, "name": "Steppin Time", "participants": {"groups":[0], "individuals":[]}},
+    {"id": 2, "name": "Holy Sound", "file": "HolySound.mp3", "participants": {}},
+]
+
+notes = [
+    {"id":0, "act_id": 2, "startTime": 0, "endTime": 10, "note": "Song starts"},
+    {"id":1, "act_id": 2, "startTime": 4, "endTime": 10, "note": "Guitar and Drums Start playing"},
+    {"id":2, "act_id": 2, "startTime": 7, "endTime": 15, "note": "Lead line"},
+    {"id":3, "act_id": 2, "startTime": 13, "endTime": 21, "note": "Lead line #2"},
+    {"id":4, "act_id": 2, "startTime": 18.2, "endTime": 25, "note": "Lead line #3"},
+    {"id":5, "act_id": 2, "startTime": 26, "endTime": 35, "note": "Verse 1"},
+    {"id":6, "act_id": 2, "startTime": 72, "endTime": 82, "note": "Verse 2"},
 ]
 
 organization = {
@@ -88,6 +99,66 @@ def update_group(id):
 
     return json.dumps(groups)
 
+
+@app.route("/acts", methods=["GET"])
+def get_acts():
+    return json.dumps(acts)
+@app.route("/acts", methods=["POST"])
+def add_acts():
+    act = request.json
+    act["id"] = (acts[-1]["id"]+1) if len(acts) > 0 else 0
+    acts.append(act)
+
+    return json.dumps(acts)
+@app.route("/acts/<int:id>", methods=["GET"])
+def get_act(id):
+    act = list(filter(lambda x: x["id"] == id, acts))[0]
+    return json.dumps(act)
+@app.route("/acts/<int:id>", methods=["PUT","POST"])
+def update_act(id):
+    old_act = list(filter(lambda x: x["id"] == id, acts))[0]
+    new_act = request.json
+
+    # Don't let the user change the id
+    new_act["id"] = id
+
+    # Update the dancer
+    old_act.update(new_act)
+
+    return json.dumps(acts)
+
+# Notes
+@app.route("/acts/<int:id>/notes", methods=["GET"])
+def get_notes(id):
+    filtered_notes = list(filter(lambda x: x["act_id"] == id, notes))
+    return json.dumps(filtered_notes)
+@app.route("/acts/<int:act_id>/notes", methods=["POST"])
+def add_note(act_id):
+    note = request.json
+    note["id"] = (notes[-1]["id"]+1) if len(notes) > 0 else 0
+    note["act_id"] = act_id
+    notes.append(note)
+
+    return json.dumps(acts)
+@app.route("/acts/<int:act_id>/notes/<int:id>", methods=["GET"])
+def get_note(act_id, id):
+    note = list(filter(lambda x: x["id"] == id and x["act_id"] == act_id, notes))[0]
+    return json.dumps(note)
+@app.route("/acts/<int:act_id>/notes/<int:id>", methods=["PUT","POST"])
+def update_note(act_id, id):
+    old_note = list(filter(lambda x: x["id"] == id and x["act_id"] == act_id, notes))[0]
+    new_note = request.json
+
+    # Don't let the user change the id
+    new_note["id"] = id
+    new_note["act_id"] = act_id
+
+    # Update the item
+    old_note.update(new_note)
+
+    return json.dumps(notes)
+
+
 @app.route("/")
 def root():
     return app.send_static_file("index.html")
@@ -97,4 +168,4 @@ def send_js(path):
     return send_from_directory("web/js", path)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0")
--- web/js/act.js
+++ web/js/act.js
@@ -2,36 +2,82 @@
 TODOs
 - Make it so comments stay visible (maybe show previous, current, and next)
 - Allow adding audio/video comments
+- Add persistence and file associations
+- Add audio upload
 */
 
 
 $(document).ready(init);
 
-function init() {
-  $("#tag-save").click(saveTag);
-
-  showNotes();
-  window.setInterval(updateAct,50);
-}
-
-let data = [
-  {id:0, startTime: 0, endTime: 10, note: "Song starts"},
+let data = [];
+/*  {id:0, startTime: 0, endTime: 10, note: "Song starts"},
   {id:1, startTime: 4, endTime: 10, note: "Guitar and Drums Start playing"},
   {id:2, startTime: 7, endTime: 15, note: "Lead line"},
   {id:3, startTime: 13, endTime: 21, note: "Lead line #2"},
   {id:4, startTime: 18.2, endTime: 25, note: "Lead line #3"},
   {id:5, startTime: 26, endTime: 35, note: "Verse 1"},
-];
+];*/
+
+let actId = 2;
+
+function init() {
+  $("#tag-save").click(saveTag);
+
+  //showNotes();
+  window.setInterval(updateAct,50);
+
+  getNotes();
+}
+
+
+function getNotes() {
+  // TODO replace with selections
+  $.get({
+    url: "/acts/"+actId+"/notes",
+    dataType: "json",
+    contentType: "application/json",
+    success: function(notes) {
+      data = notes;
+      showNotes();
+    }
+  });
+}
+
 
 function showNotes() {
   entries = data.sort(function(a,b){return a.startTime-b.startTime});
   $("#act-info").empty();
   for (i in entries) {
     entry = entries[i];
-    $noteEntry = $("<div class='note'>"+entry.startTime + " - " + entry.note+"</div>");
+    $noteEntry = $("<div class='note' note-id='"+entry.id+"'>"+formatTime(entry.startTime) + " - " + entry.note+"</div>");
     $noteEntry.attr("id", "note-"+entry.id);
+    $noteEntry.click(handleNoteClick);
     $("#act-info").append($noteEntry);
   }
+}
+
+function handleNoteClick(event) {
+  let target = event.target;
+  let $target = $(target);
+  let entry = data.filter(function (item){return item.id == $target.attr("note-id")})[0];
+  
+  moveAudioLocation(entry.startTime);
+}
+
+function moveAudioLocation(time) {
+  document.getElementById("audio-player").currentTime = time;
+}
+
+function formatTime(fullSeconds) {
+  let minutes = Math.floor(fullSeconds/60);
+  let seconds = Math.floor(fullSeconds) - (minutes*60);
+  let millis = Math.round((fullSeconds - Math.floor(fullSeconds))*1000);
+
+  let paddedMinutes = String(minutes).padStart(2, '0');
+  let paddedSeconds = String(seconds).padStart(2, '0');
+  let paddedMillis = String(millis).padEnd(3, '0');
+
+  return paddedMinutes + ":" + paddedSeconds + "." + paddedMillis;
 }
 
 
@@ -39,7 +85,7 @@ let highlightClass = "highlighted";
 function updateAct() {
   let position = document.getElementById("audio-player").currentTime;
 
-  entries = data.filter(function(a){return position>a.startTime && position<a.endTime});
+  entries = data.filter(function(a){return position>=a.startTime && position<a.endTime});
   entries = entries.sort(function(a,b){return a.startTime-b.startTime});
 
   $(".note").removeClass(highlightClass);
@@ -56,12 +102,22 @@ function saveTag() {
   let endTime = startTime + 10;
   let note = $("#tag-note").val();
 
-  data.push({
+
+  noteData = {
     startTime:startTime,
     endTime:endTime,
-    note:note,
-    id: new Date().getTime()
+    note:note
+  };
+
+  
+  $.ajax({
+    method: "POST",
+    url: "/acts/"+actId+"/notes",
+    data: JSON.stringify(noteData),
+    dataType: "json",
+    contentType: "application/json",
+    success: getNotes
   });
 
-  showNotes();
+  //showNotes();
 }
