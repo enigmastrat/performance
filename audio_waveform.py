4d837457d5de9d8a3abf145b4fc51f9ad14ceb83
Robert Reed <robbie@cypheye.com>
2023-05-14 16:03:08 -0700

    Added audio_waveform.py generation

---
+++ audio_waveform.py
@@ -0,0 +1,39 @@
+import librosa
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def generate_waveform(file_path, output_file_name="testplot.png"):
+    file = librosa.load(file_path, sr=750)
+    audio = file[0]
+    sample_rate = file[1]
+    n_samples = len(audio)
+    t_audio = n_samples/sample_rate
+
+    # Not sure if this really does anything here...
+    signal_array = np.frombuffer(audio, dtype=np.float32)
+
+    times = np.linspace(0, t_audio, n_samples)
+
+
+    plt.figure(figsize=(40, 2.5))
+    plt.plot(times, audio)
+    #plt.title('audio')
+    #plt.ylabel('Signal Value')
+    #plt.xlabel('Time (s)')
+    plt.xlim(0, t_audio)
+    plt.grid(False)
+    plt.axis('off')
+
+    plt.subplots_adjust(top = 1, bottom = 0, right = 1, left = 0, hspace = 0, wspace = 0)
+    plt.margins(0,0)
+
+
+    #plt.show()
+    plt.savefig(output_file_name, transparent = True,)
+
+def main():
+    generate_waveform("web/HolySound.mp3")
+
+if __name__ == "__main__":
+    main()
